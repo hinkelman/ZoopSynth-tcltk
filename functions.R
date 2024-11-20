ffbad8ab0b16685d92cd61c142caf1849a96962e
Travis Hinkelman <thinkelman@pm.me>
2024-11-20 05:58:43 -0800

    Switch to line plots and use tidyr::complete in prep_samples

--- functions.R
+++ functions.R
@@ -1,17 +1,24 @@
 
 prep_samples <- function(data){
-  data |> 
-    mutate(Month = factor(month.abb[month(Date)], levels = month.abb)) |> 
+  tmp = data |>
+    mutate(MonthNum = month(Date)) |> 
     filter(CPUE > 0) |> 
-    select(Source, Year, Month, SampleID) |> 
+    select(Source, Year, MonthNum, SampleID) |> 
     distinct() |> 
-    group_by(Source, Year, Month) |> 
+    group_by(Source, Year, MonthNum) |> 
     summarise(N_samples = n(), .groups = "drop")
+  
+  tmp |> 
+    mutate(Month = factor(month.abb[MonthNum], 
+                          levels = month.abb[sort(unique(tmp$MonthNum))])) |> 
+    select(-MonthNum) |> 
+    tidyr::complete(Source, Year, Month, fill = list(N_samples = NA_integer_))
 }
 
 plot_samples <- function(data, source_colors){
-  p = ggplot(data, aes(x=Year, y = N_samples, fill = Source)) +
-    geom_bar(stat = "identity") +
+  p = ggplot(data, aes(x=Year, y = N_samples, color = Source)) +
+    geom_line() +
+    geom_point() +
     facet_wrap(~ Month) +
     coord_cartesian(expand = 0) +
     scale_x_continuous(breaks = function(x) unique(floor(pretty(seq(min(x), max(x)), n = 4))), expand = c(0, 0)) +
@@ -21,6 +28,6 @@ plot_samples <- function(data, source_colors){
           strip.background = element_blank(), 
           text = element_text(size = 14), 
           panel.spacing.x = unit(15, "points")) +
-    scale_fill_manual(name = "Source", values = source_colors)
+    scale_color_manual(name = "Source", values = source_colors)
   plot(p)
 }
